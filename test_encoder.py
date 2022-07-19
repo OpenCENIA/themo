9d68e439060cca9001f1408a4d7e72d47925f7da
Eugenio Herrera <eiherrera@uc.cl>
2022-07-19 16:58:27 -0400

    Update test_encoder.py

--- test_encoder.py
+++ test_encoder.py
@@ -4,4 +4,6 @@ import themo.embedder as embedder
 dset = data.WITParallel(datadir="/home/ouhenio/storage/datasets/themo", split="val", download=True)
 clipEmbedder = embedder.FrozenCLIPEmbedder()
 
+encoded = clipEmbedder.encode(dset[0][1])
+
 print(dset[0])
