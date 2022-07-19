d8f584ec4b935baf4712016c41a1d48eb6b63e8c
Eugenio Herrera <eiherrera@uc.cl>
2022-07-19 18:01:31 -0400

    Update test_encoder.py

--- test_encoder.py
+++ test_encoder.py
@@ -6,6 +6,9 @@ clipEmbedder = embedder.FrozenCLIPEmbedder().cuda()
 
 encoded = clipEmbedder.encode(dset[0][1])
 
-print(encoded)
 
-print(dset[0])
+print(f"Prompt: '{dset[0][1]}'\n")
+print("CLIP embedding:")
+print(encoded)
+print("\n")
+print(f"embedding dimensions: {encoded.shape}")
