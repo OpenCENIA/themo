68543f8ffcba9e405937516f5c2d9ce662b0ab80
Eugenio Herrera <eiherrera@uc.cl>
2022-07-21 11:14:59 -0400

    Update test_encoder.py

--- test_encoder.py
+++ test_encoder.py
@@ -5,7 +5,7 @@ import torch
 dset = data.WITParallel(datadir="/home/ouhenio/storage/datasets/themo", split="val", download=True)
 clipEmbedder = embedder.FrozenCLIPEmbedder().cuda()
 
-encoded = clipEmbedder.encode(dset[0][1])
+encoded, pooled_encoded = clipEmbedder.encode(dset[0][1])
 
 
 print(f"Prompt: '{dset[0][1]}'\n")
@@ -13,7 +13,8 @@ print("CLIP embedding:")
 print(encoded)
 print("\n")
 print(f"embedding dimensions: {encoded.shape}")
+print(f"EOS embedding dimensions: {pooled_encoded.shape}")
 
-print(torch.equal(encoded[0][1], encoded[0][0]))
-print(torch.equal(encoded[0][1], encoded[0][-1]))
+# print(torch.equal(encoded[0][1], encoded[0][0]))
+# print(torch.equal(encoded[0][1], encoded[0][-1]))
 
