49bbd595de8c7bf7db17e6f27144c8156194241f
Gabriel Chaperon <gabrielchaperonb@gmail.com>
2022-07-25 18:52:20 -0400

    Add train command and click dep

---
+++ themo/__init__.py
@@ -0,0 +1,4 @@
+from .data import WITParallel, WITParallelDataModule
+from .model import ThemoTextModel, ThemoTextLitModel
+
+__all__ = ["WITParallel", "WITParallelDataModule" "ThemoTextModel", "ThemoTextLitModel"]
